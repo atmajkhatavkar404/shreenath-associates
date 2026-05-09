9471467f3417ee03226f17034f9dd920f23b4fd8
brandifymedia18 <brandifymedia18@gmail.com>
2026-05-09 19:15:50 +0530

    Fix build errors in HomeStats and eslint config

--- components/home/HomeStats.tsx
+++ components/home/HomeStats.tsx
@@ -89,7 +89,6 @@ export default function HomeStats() {
                   <Counter value={stat.value} suffix={stat.suffix} />
                 </h3>
                 <p className="text-xl font-semibold text-gray-800">{stat.description}</p>
-                <p className="text-sm text-gray-500 mt-1">{stat.name}</p>
               </motion.div>
             );
           })}
